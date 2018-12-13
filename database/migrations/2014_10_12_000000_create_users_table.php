5392b37b0597c09d0450b3d303e3873f913f5d52
DaiYuSs <daiyuss@126.com>
2018-12-13 20:45:10 +0800

    Database initialization

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersInformations extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersInformations extends Migration
      */
     public function up()
     {
-        Schema::create('users_informations', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('email')->unique();
             $table->string('password');
-            $table->string('email');
-            $table->boolean('competence');
-            $table->dateTime('create_time');
-            $table->timestamp('modify _time');
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
@@ -31,6 +30,6 @@ class CreateUsersInformations extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users_informations');
+        Schema::dropIfExists('users');
     }
 }
